30dcf9dbc5c6869f0446e0da54c715ac5caa25f4
nec <nec@activeviam.com>
2023-08-24 11:41:29 +0200

    Implement getter and setter

--- udaf/src/main/java/com/activeviam/databricks/udaf/MyAverage.java
+++ udaf/src/main/java/com/activeviam/databricks/udaf/MyAverage.java
@@ -21,8 +21,26 @@ public static class Average implements Serializable  {
 	private long sum;
 	private long count;
 
-	// Constructors, getters, setters...
+	public Average(long sum, long count) {
+		this.sum = sum;
+		this.count = count;
+	}
 
+	public long getSum() {
+		return this.sum;
+	}
+
+	public long getCount() {
+		return this.count;
+	}
+
+	public void setSum(long sum) {
+		this.sum = sum;
+	}
+
+	public void setCount(long count) {
+		this.count = count;
+	}
 }
 
 public static class MyAverage extends Aggregator<Long, Average, Double> {
@@ -65,4 +83,5 @@ public static class MyAverage extends Aggregator<Long, Average, Double> {
 	public Encoder<Double> outputEncoder() {
 		return Encoders.DOUBLE();
 	}
+
 }
